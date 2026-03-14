2355cd973d9e656891e5be84c4b530357fd60682
RebeccaCasw-21 <rebeccacaswell21@gmail.com>
2026-03-13 19:22:13 -0500

    Add TaskList component to render task items

---
+++ Src/Components/TaskList.tsx
@@ -0,0 +1,23 @@
+import TaskItem from "./TaskItem"
+import { Task } from "../models/Task"
+
+interface Props {
+  tasks: Task[]
+  onToggle: (id: string) => void
+  onDelete: (id: string) => void
+}
+
+export default function TaskList({ tasks, onToggle, onDelete }: Props) {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onToggle={onToggle}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  )
+}
